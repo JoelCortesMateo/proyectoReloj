887f0834644f188528e62c61f026281cf9575587
Joel <joel.cortes@educamadrid.org>
2023-02-15 21:28:32 +0100

    La ventana del temporizador es funcional y sin ningun error.

--- proyectoReloj/Temporizador.xaml.cs
+++ proyectoReloj/Temporizador.xaml.cs
@@ -28,13 +28,14 @@ namespace proyectoReloj
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromSeconds(1);
             timer.Tick += Timer_Tick;
+            Restart.IsEnabled= false;
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
             time--;
-            timerLabel.Content = time.ToString();
-            if (time == 0)
+            tempo.Content = time.ToString();
+            if (time <= 0)
             {
                 timer.Stop();
                 MessageBox.Show("¡TIEMPO!");
@@ -43,20 +44,44 @@ namespace proyectoReloj
 
         private void startButton_Click(object sender, RoutedEventArgs e)
         {
-            time = int.Parse(timeTextBox.Text);
-            timer.Start();
+            Restart.IsEnabled = false;
+            if(time == 0) 
+            { 
+                time = int.Parse(timerTextBox.Text);
+                timer.Start();
+                timerTextBox.Text = "0";
+            }
+            else if(time > 0)
+            {
+                timer.Start();
+            }
         }
 
         private void stopButton_Click(object sender, RoutedEventArgs e)
         {
             timer.Stop();
+            Restart.IsEnabled= true;
         }
 
         private void resetButton_Click(object sender, RoutedEventArgs e)
         {
             time = 0;
-            timerLabel.Content = "0";
+            tempo.Content = "0";
             timer.Stop();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            Hide();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Cronometro crono = new Cronometro();
+            crono.Show();
+            Hide();
         }
     }
 }
